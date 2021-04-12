6713531bb7a841fc39af23c171e274c1d94c8509
Matt Taylor <m.c.t@hotmail.co.uk>
2021-04-12 13:39:47 +0100

    Using correct API link now

--- windsolardata.py
+++ windsolardata.py
@@ -8,12 +8,15 @@ Created on Sun Apr 11 19:54:45 2021
 import requests as rq
 import xml.etree.ElementTree as ET
 import numpy as np
+import matplotlib.pyplot as plt
+from datetime import date
+
 
 APIKey = 'g0t43vms7e1tlk9'
-Date = '2021-04-02'
-Period = '1'
+Date = '2021-04-08'
+Period = '24'
 # x = rq.get('https://api.bmreports.com/BMRS/STORAW/?APIKey=g0t43vms7e1tlk9&FromSettlementDate=2014-12-31&ServiceType=xml/XML')
-ws = rq.get('https://api.bmreports.com/BMRS/B1630/V1?APIKey='+ APIKey +'&SettlementDate=' + Date +'&Period='+ Period +'&ServiceType=xml')
+ws = rq.get('https://api.bmreports.com/BMRS/B1620/V1?APIKey='+ APIKey +'&SettlementDate=' + Date +'&Period='+ Period +'&ServiceType=xml')
 #%%
 ws = ws.text
 wind_root = ET.fromstring(ws)
@@ -21,9 +24,14 @@ wind_root = ET.fromstring(ws)
 regen = np.asarray([['fuel', 'power']], dtype = object)
 
 for item in wind_root[1][1]:
-    fuel = np.asarray((item[2].text, item[5].text))
+    fuel = np.asarray((item[9].text, item[4].text))
     fuel = np.expand_dims(fuel, axis=0)
     regen = np.append(regen, fuel, axis = 0)
     
 print(regen)
+
+solarplot = np.asarray(regen[1:])
+plt.bar(np.arange(0, np.shape(solarplot)[0]), np.float32(solarplot[:,1]))
+plt.xticks(np.arange(0, np.shape(solarplot)[0]), solarplot[:,0], rotation = 'vertical')
+
 #%%
